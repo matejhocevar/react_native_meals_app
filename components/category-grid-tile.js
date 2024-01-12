11064f8ad28ab6a8ef6694f3071767b7e2896cf8
Matej Hočevar <matej.hocevar@tretton37.com>
2024-01-12 11:48:09 +0100

    Add categories grid

---
+++ components/category-grid-tile.js
@@ -0,0 +1,58 @@
+import {Platform, Pressable, StyleSheet, Text, View} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import {shadeColor} from "../utils/color";
+
+const CategoryGridTile = ({title, color}) => {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{color: "#cccccc"}}
+        style={
+          ({pressed}) => [
+            styles.button,
+            pressed ? styles.buttonPressed : null
+          ]}>
+        <LinearGradient
+          style={styles.innerContainer}
+          colors={[shadeColor(color, 20), color]}>
+          <Text style={styles.title}>{title}</Text>
+        </LinearGradient>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+    overflow: Platform.select({android: "hidden", ios: "visible"}),
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18
+  }
+});
+
+export default CategoryGridTile;
